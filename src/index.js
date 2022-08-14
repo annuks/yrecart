6704a825e96a8562abf4a99554b8c8f3bdcd3117
Annu Kumar Singh <annukumarsingh.aks@gmail.com>
2022-08-14 12:54:50 +0530

    Firebase Configuration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_MW_cHTJYjNhwJSSUqLCAxdYLkxBmIE4",
+  authDomain: "cart-6969a.firebaseapp.com",
+  projectId: "cart-6969a",
+  storageBucket: "cart-6969a.appspot.com",
+  messagingSenderId: "300369230640",
+  appId: "1:300369230640:web:c613ec0e3d87eac6d566f5"
+};
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
